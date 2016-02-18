40bd5ee103c1122221834d0c2138beac7fcba876
LinDayZ <mr.agf@inbox.ru>
2016-02-19 01:02:44 +0300

    new

---
+++ OriginalWayOfHope/WayOfHope/Assets/WayOfHope/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿//Скрипт описывающий простуб логику преследования игрока противником 
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	// определяем настраиваем переменые
+	public Transform target;//Цель
+	public int moveSpeed;// скрость перемещения
+	public int rotationSpeed;//скорость поворота
+	public Transform myTransform;//временная переменная для хранения ссылки на свойство transform (это оптимизации)
+
+	void Awake(){
+		//ссылаемся на свойство transform, чтобы сократить время обращения к мему в теле скрипта
+		myTransform=transform;
+	}
+	//начальная оптимизация 
+	void Start () {
+	//ищем объект по тегу Player
+		GameObject go=GameObject.FindGameObjectWithTag("Player");
+		//и ставим на него прицел (делаем нашей целью)
+		target=go.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//чертим линию  от нас к игроку (видно только в редакторе)
+		Debug.DrawLine(target.position,myTransform.position,Color.yellow);
+		//поварачиваемся в сторону игрока(цели)
+		myTransform.rotation=Quaternion.Slerp(myTransform.rotation,Quaternion.LookRotation(target.position-myTransform.position),rotationSpeed * Time.deltaTime);
+		myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ OriginalWayOfHope/WayOfHope/Assets/WayOfHope/Scripts/PlayAttack.cs
@@ -0,0 +1,31 @@
+﻿//Атака игрока с нанесние урона Enemy
+using UnityEngine;
+using System.Collections;
+
+public class PlayAttack : MonoBehaviour {
+	//Определение публичных переменных
+	public GameObject target;//цель игрока
+	// Use this for initialization
+	void Start () {
+	
+	}
+	//цикл на каждый кадр
+	
+	// Update is called once per frame
+	void Update () {
+	//по нажатию клавиши F на клавиатуре происходит атака по Enemy
+		if(Input.GetKeyUp(KeyCode.F)){
+			Attack ();
+		}
+	}
+	//атака Enemy
+	private void Attack(){
+
+		Debug.Log ("Player Attacking");
+		//У цели Enemy ищим компонет с именем EnemyHealth - скрипт
+		EnemyHealth eh=(EnemyHealth)target.GetComponent("EnemyHealth");
+		//и используем его метод изменения состояния здоровья 
+		//(необходимо переписать AddjustCurrentHeath с учетом новых требований)
+		eh.AddjustCurrentHealth(-10);
+	}
+}
